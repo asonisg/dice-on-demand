3c0f3a55d27f5a6fe5afa19595f82810c3a31503
Abhishek Soni <43876822+asonisg@users.noreply.github.com>
2021-03-22 22:06:40 +0800

    Create deploy.sh
    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -xe # Stop script from running if there are any errors
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh ip172-18-0-57-c1c9n20h550g00di06sg@direct.labs.play-with-docker.com <<EOF
+  docker run --name mynginx1 -p 80:80 -d nginx  # Change to whatever commands you need!
+EOF
