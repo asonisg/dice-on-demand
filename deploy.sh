b2cff44461d5ae34b93bf0287a58efaa687d4981
Abhishek Soni <43876822+asonisg@users.noreply.github.com>
2021-03-22 23:02:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 set -xe # Stop script from running if there are any errors
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -o StrictHostKeyChecking=no ip172-18-0-57-c1c9n20h550g00di06sg@direct.labs.play-with-docker.com <<EOF
+ssh -o StrictHostKeyChecking=no -i id_rsa ip172-18-0-57-c1c9n20h550g00di06sg@direct.labs.play-with-docker.com <<EOF
   docker run --name mynginx1 -p 80:80 -d nginx  # Change to whatever commands you need!
 EOF
